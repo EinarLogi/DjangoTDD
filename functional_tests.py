55daec51b5cae5de5a5969928039aba94cbba1e7
einarlogi <einarlogi14@gmail.com>
2015-06-20 12:20:01 +0000

    we can add one list item but second item fails as expected

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -32,22 +33,29 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy peacock feathers" as an item in a to-do list
      
         inputbox.send_keys(Keys.ENTER)
+
+        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         #There is still a text box inviting her to add another item. you
         #enters "Use peacock feathers to make a fly" 
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER) 
         #The page updates again, and now shows both items on your list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         #You wonder whether the site will remember the list. You see that the site has
         #generated a unique URL for you  -- there is some 
         #explanatory text to that effect
-
+        self.fail('Finish the test!')
         #You visit that URL - your to-do list is still there.
 
 if __name__ == '__main__':
